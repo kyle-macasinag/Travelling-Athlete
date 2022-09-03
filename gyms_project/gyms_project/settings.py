cc46ccade52b276807c1849db5f518648c9ad3d7
kyle.macasinag <kylenmac@verizon.net>
2022-09-03 15:46:49 -0400

    CHORE: Migrations made

--- gyms_project/gyms_project/settings.py
+++ gyms_project/gyms_project/settings.py
@@ -113,3 +113,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from gyms_project.local_settings import *
+except ImportError:
+    pass
